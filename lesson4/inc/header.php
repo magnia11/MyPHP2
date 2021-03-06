bda089b46b452ac042690237a8f2393d89e3d25f
Anna_Rimskaya <magnia11@yandex.ru>
2021-03-06 18:59:56 +0300

    сделала 4 дз

---
+++ lesson4/inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Каталог товаров</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<header></header>
+	<main>
+		
